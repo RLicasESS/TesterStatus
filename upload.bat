c9d11c72c97f6d35a03766313231811f275e98b8
Richard Licas <rlicas@esstech.com>
2025-05-16 12:59:06 -0700

    Update index.html

---
+++ upload.bat
@@ -0,0 +1,7 @@
+cd C:\Users\RLicas\Documents\LOCAL_DRIVE\PYTHON\TESTERMONITOR\TesterStatus
+git status
+git add .
+git status
+git commit -m "Update index.html" || echo "Nothing to commit"
+git push origin main
+pause
